e18d587acf3d7857acce2b3f66a2cc42a2fc55d2
Klysman Rezende <42696664+klysman08@users.noreply.github.com>
2020-07-19 19:50:59 -0300

    up

--- app05_quiz_de_perguntas/lib/main.dart
+++ app05_quiz_de_perguntas/lib/main.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    
+    print(perguntaSelecionada);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual sua cor favorida?',
+      'Qual seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Text("Olá flutter!"),
-        
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class PerguntaApp extends StateFulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
